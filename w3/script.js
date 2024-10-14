a84a7d19280ed9c0989dd45d272d176fc80f8793
Benedict <mokayaakivatsi@gmail.com>
2024-10-14 17:47:47 +0300

    js output

--- w3/script.js
+++ w3/script.js
@@ -1,3 +1,6 @@
 function displayTime(){
     document.getElementById('demo').innerHTML =new Date();
+    document.getElementById("sum").innerHTML = 40 + 29;
+    window.alert("Hey pookiee 💋 💋,the time is on your left.");
+    
 }
